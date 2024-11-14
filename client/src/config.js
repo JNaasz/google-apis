695e656bedff67104b33c63cce5007974eb56a2c
Jennifer Naasz <naasz.jl@gmail.com>
2024-11-14 12:42:26 -0500

    add npm scripts for serve and build

--- client/src/config.js
+++ client/src/config.js
@@ -1,5 +1,5 @@
-// const BASE_SERVER_URL = 'http://localhost:2000'; // local server
-const BASE_SERVER_URL = 'http://192.168.69.174:2000/'; // remote server
+const BASE_SERVER_URL = 'http://localhost:2000'; // local server
+// const BASE_SERVER_URL = 'http://192.168.69.174:2000/'; // remote server
 
 const endpoints = {
 		sheets: `${BASE_SERVER_URL}/sheet-data`,
